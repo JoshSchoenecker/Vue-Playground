c8a2b9f33671e7cf6e5ecb2517553e7a0730da62
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-13 15:14:36 -0600

    finished with challenges in altered files

--- src/components/ExpressionsExercise.vue
+++ src/components/ExpressionsExercise.vue
@@ -7,13 +7,15 @@
                     <p>Write a math equation Vue can evaluate.</p>
 
                     <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
+                     (7+7) - (7 + 7)    
+                    }}</p>
                 </div>
                 <div class="border p-1">
                     <p>Use interpolation to get the customary Hello, World! to display on the screen. Don't
                         forget your quotes</p>
                     <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
+                     "Hello, World!"
+                    }}</p>
                 </div>
             </div>
         </div>
